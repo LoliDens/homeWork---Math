cd14d2eefc91781337aad8523f336a0f0295d4fd
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-14 18:06:56 +0500

    Добавьте файлы проекта.

---
+++ homeWork - Math/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork___Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountPicture = 52;
+            int pictureInRow = 3;
+            int numberOfRows = amountPicture / pictureInRow;
+            int picturesOutsideTheRow = amountPicture % pictureInRow;
+
+            Console.WriteLine($"Колличество рядов заполеных картинками : {numberOfRows} \n" +
+                $"Колличество кортинок сверз меры: {picturesOutsideTheRow}");
+            Console.ReadKey();
+
+        }
+    }
+}
